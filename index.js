be2225c9be08b1c47c848b211f4e01084cdf8c09
Amirmazed <76693565+Amirmazed@users.noreply.github.com>
2022-01-05 01:36:18 +0600

    correction

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+//connection test
 async function run() {
 
      try{
